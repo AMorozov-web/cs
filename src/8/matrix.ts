a1708220835b19b4bad1ff54607a1e0a68fbce76
Alex Morozov <alex.morozov.work@mail.ru>
2024-04-18 23:04:24 +0300

    completed matrix

--- src/8/matrix.ts
+++ src/8/matrix.ts
@@ -1,23 +1,89 @@
-class Matrix {
-    buffer: any
+type MatrixArrayTypes =
+    | Uint8ClampedArray
+    | Uint8Array
+    | Int8Array
+    | Uint16Array
+    | Int16Array
+    | Uint32Array
+    | Int32Array
+    | Float32Array
+    | Float64Array
+    | BigUint64Array
+    | BigInt64Array;
 
-    constructor(array: any, ...dimensions: number[]) {
+type MatrixArrayConstructor<T extends MatrixArrayTypes> = new (capacity: number) => T;
 
+type MatrixArrayValue<T extends MatrixArrayTypes> = T extends (BigUint64Array | BigInt64Array) ? bigint : number;
+
+interface ChangeMultiplyOrderFunction {
+    (prevOrder: number[]): number[];
+}
+
+class Matrix<T extends MatrixArrayTypes> {
+    private readonly array: T;
+    private readonly dimensions : number[];
+    private multiply: number[];
+
+
+    constructor(TypedArrayConstructor: MatrixArrayConstructor<T>, ...dimensions: number[]) {
+        if (dimensions.some((value) => value < 1 || value % 1 > 0)) {
+            throw new TypeError('Each dimension must be greater or equal than 1 and no contain fractional values');
+        }
+
+        const capacity = dimensions.reduce((result, current) => result * current, 1);
+
+        this.array = new TypedArrayConstructor(capacity);
+        this.dimensions = dimensions;
+        //Заранее рассчитываем множители для каждой из координат по порядку - каждая координата, кроме первой,
+        //будет умножаться на произведение размеров предшествующих ей осей. Множитель первой - 1
+        this.multiply = dimensions.map((_, index) => index ? dimensions.slice(0, index).reduce((a, b) => a * b, 1) : 1);
     }
 
-    set(...dimensions: number[], value: number) {
-
+    /**
+     * Позволяет изменить порядок множителей, что в конечном итоге меняет расположение элементов в памяти,
+     * соответственно и их порядок обхода итератором
+     */
+    setMultiplyOrder(func: ChangeMultiplyOrderFunction): void {
+        this.multiply = func(this.multiply);
     }
 
-    get(...dimensions: number[]) {
+    set(...args: [...coords: number[], value: MatrixArrayValue<T>]): void {
+        const coords = args.slice(0, -1) as number[];
+        const value = args.pop() as MatrixArrayValue<T>;
 
+        const index = this.getIndex(coords);
+
+        this.array[index] = value;
+    }
+
+    get(...coords: number[]): MatrixArrayValue<T> {
+        const index = this.getIndex(coords);
+
+        return this.array[index] as MatrixArrayValue<T>;
+    }
+
+    get buffer(): ArrayBuffer {
+        return this.array.buffer;
+    }
+
+    *values(): IterableIterator<MatrixArrayValue<T>> {
+        for (const value of this.array) {
+            yield value as MatrixArrayValue<T>;
+        }
+    }
+
+    private getIndex(coords: number[]): number {
+        if (coords.length !== this.dimensions.length) {
+            throw new RangeError('Extra coordinates found');
+        }
+
+        return coords.reduce((result, current, index) => result + current * this.multiply[index], 0);
     }
 }
 
-
-
-
 const matrix2n2n2 = new Matrix(Int32Array, 2, 2, 2);
+
+matrix2n2n2.setMultiplyOrder(([a, b, c]) => [c, a, b]); // Меняем множители таким образом, чтобы значения шли подряд в итераторе
 
 matrix2n2n2.set(0, 0, 0, 1);
 matrix2n2n2.set(0, 1, 0, 2);
@@ -40,3 +106,4 @@ matrix2n2n2.get(1, 0, 1); // 7
 matrix2n2n2.get(1, 1, 1); // 8
 
 console.log(matrix2n2n2.buffer);  // Ссылка на ArrayBuffer
+console.log(Array.from(matrix2n2n2.values()));
