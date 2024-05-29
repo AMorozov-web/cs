2da9582893251d0ba33a0ff977300ea8001d0e8c
Alex Morozov <alex.morozov.work@mail.ru>
2024-05-29 22:19:08 +0300

    added random

---
+++ src/21/random.ts
@@ -0,0 +1,26 @@
+const random = (min: number, max: number): IterableIterator<number> => {
+    const get = (): number => {
+        return Math.floor(Math.random() * (max - min) + min);
+    }
+
+    return {
+        [Symbol.iterator]() {
+            return this;
+        },
+
+        next(): IteratorResult<number> {
+            return {
+                done: false,
+                value: get(),
+            }
+        }
+    }
+}
+
+
+const randomInt = random(0, 100);
+
+console.log(randomInt.next());
+console.log(randomInt.next());
+console.log(randomInt.next());
+console.log(randomInt.next());
