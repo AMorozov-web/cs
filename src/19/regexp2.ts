314322da300b33e7b60da2a6ccf182c4dc333f04
Alex Morozov <alex.morozov.work@mail.ru>
2024-05-29 21:17:54 +0300

    added unique and findMoney

--- src/19/regexp2.ts
+++ src/19/regexp2.ts
@@ -18,11 +18,23 @@ const zipStr = (str: string): string => {
 
 console.log(zipStr('abbaabbafffbezza')); // abafbeza
 
+////////////////////////
 
+const unique = (str: string): string => {
+    const regexp = /(.)(?:.*)(\1)/g;
 
-// console.log(unique('abaceffgw')); // bcegw
-//
-//
-//
-// // ['100 00,53$', '500₽']
-// console.log(findMoney(`20.10.2020 Федор взял у меня 100 00,53$ и обещался вернуть не поздее 25 числа, но уже через 2 дня, он занял еще 500₽`));
+    return str.replace(regexp, (subStr, value) => subStr.replaceAll(value, ''));
+}
+
+console.log(unique('abaceffgw')); // bcegw
+
+///////////////////////
+
+const findMoney = (str: string): string[] => {
+    const regexp = /((\d+)( \d+?)?(\,\d+)?(\$|₽))/g;
+
+    return [...str.matchAll(regexp)].map(([subStr]) => subStr);
+}
+
+// ['100 00,53$', '500₽']
+console.log(findMoney(`20.10.2020 Федор взял у меня 100 00,53$ и обещался вернуть не поздее 25 числа, но уже через 2 дня, он занял еще 500₽`));
